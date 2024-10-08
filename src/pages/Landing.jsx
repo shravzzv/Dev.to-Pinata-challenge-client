228f120e246459176f0f18c0fddabe3c4f551f0c
sai shravan <saishravan384@gmail.com>
2024-10-08 10:54:50 +0530

    feat: Add hero section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,20 @@
+import '../styles/Landing.css'
+
 export default function Landing() {
   return (
     <div className='landing'>
-      <p>hello world</p>
+      <div className='hero'>
+        <h1>Get your next big time ideas</h1>
+        <p>
+          Our platform is your treasure trove for fresh, innovative concepts.
+          Discover the inspiration you need to fuel your next big project. Start
+          exploring today and let your creativity soar.
+        </p>
+        <div className='buttons'>
+          <button className='cta'>Sign Up</button>
+          <button className='cta'>Sign in</button>
+        </div>
+      </div>
     </div>
   )
 }
